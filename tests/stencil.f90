3766ca3d2d30222a5233d2b181ef85d7633835ad
Jan H <j.c.hueckelheim@qmul.ac.uk>
2019-01-08 23:11:36 +0000

    added problematic test case

--- tests/stencil.f90
+++ tests/stencil.f90
@@ -19,3 +19,14 @@ subroutine stencil_default(res, u, n)
     r(i) = u(i-1) + u(i+1) -2*u(i)
   end do
 end subroutine
+
+subroutine stencil_problem(u, n)
+  integer :: n, i
+  double precision, dimension(2,n) :: u
+  !---------------------------------------
+  continue
+  !$omp parallel for
+  do i=2,n-1
+    u(1,i) = u(2,i-1) + u(2,i+1) -2*u(2,i)
+  end do
+end subroutine
